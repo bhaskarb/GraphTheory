a01140eab2ecca4e2f210dae0183ef813f75ce01
bhaskarb <bhaskar.bharath@gmail.com>
2018-10-22 18:57:28 -0400

    Added a faster heap

--- Heap.py
+++ Heap.py
@@ -22,81 +22,52 @@ class BinaryHeap(Heap):
     def __init__(self):
         self.data = []
         self.index = {}
-        self.size = 0
-
-    def _parent(self, i):
-        if i == 0 or self.empty():
-            return -1
-        return (i-1)//2
-
-    def _child(self, i, left=True):
-        if self.empty():
-            return -1
-        if left:
-            j = i*2 + 1
-        else:
-            j = i*2 + 2
-        if j < self.size:
-            return j
-        return -1
-    
-    def empty(self):
-        return self.size == 0
 
     def _downheap(self, ci=0):
-        li = self._child(ci, True)
-        ri = self._child(ci, False)
+        li = 2*ci + 1 
         ckey, citem = self.data[ci]
-        mi = ci
-        #logger.debug(ci, ckey, citem)
-        if li >=0 and ri >=0:
+        size = len(self.data)
+        while(li < size): 
             lkey, litem = self.data[li]
-            rkey, ritem = self.data[ri]
-            #logger.debug( li, lkey, litem)
-            #logger.debug( ri, rkey, ritem)
-            if lkey < ckey and lkey <= rkey:
-                mi, mkey, mitem = li, lkey, litem
-            elif rkey < ckey and rkey  < lkey:
-                mi, mkey, mitem = ri, rkey, ritem
-        elif li >=0:
-            lkey, litem = self.data[li]
-            #logger.debug( li, lkey, litem)
-            if lkey < ckey:
-                mi, mkey, mitem = li, lkey, litem
-        elif ri >=0:
-            rkey, ritem = self.data[ri]
-            #logger.debug( ri, rkey, ritem)
-            if rkey < ckey:
-                mi, mkey, mitem = ri, rkey, ritem
-        if mi != ci:
+            mi, mkey, mitem = li, lkey, litem
+            if li + 1 < size:
+                rkey, ritem = self.data[li + 1]
+                if rkey < mkey:
+                    mi, mkey, mitem = li + 1, rkey, ritem
             self.data[ci] = (mkey, mitem)
-            self.data[mi] = (ckey, citem)
             self.index[mitem] = ci
-            self.index[citem] = mi
-            self._downheap(mi)
-   
+            ci = mi
+            li = 2*ci + 1
+        self.index[citem] = ci
+        self.data[ci] = (ckey, citem)
+        self._upheap(ci)
+    
     def _upheap(self, ci):
-        pi = self._parent(ci)
-        if pi >= 0:
+        if ci == 0:
+            return
+        ckey, citem = self.data[ci]
+        while ci > 0:
+            pi = (ci - 1)//2
             pkey, pitem = self.data[pi]
-            ckey, citem = self.data[ci]
-        #    #logger.debug ci, pi,ckey, pkey
-            if ckey < pkey:
-                self.data[pi] = ckey, citem
-                self.data[ci] = pkey, pitem
-                assert ci != pi
-                self.index[pitem] = ci
-                self.index[citem] = pi
-                self._upheap(pi)
+            if pkey <= ckey:
+                break
+            self.index[pitem] = ci
+            self.data[ci] = pkey, pitem
+            ci = pi
+        self.index[citem] = ci
+        self.data[ci] = ckey, citem
 
     def _update(self, key, item):
         index = self.index[item]
+        (okey, oitem) = self.data[index]
         self.data[index] = (key, item)
-        #logger.debug("ORIG:UPDATE: %s, %s" %(str(self.index), str(self.data[0:self.size])))
-        self._upheap(index)
-        #logger.debug("UP:UPDATE: %s, %s" %(str(self.index), str(self.data[0:self.size])))
-        self._downheap(index)
-        #logger.debug("DOWN:UPDATE: %s, %s" %(str(self.index), str(self.data[0:self.size])))
+        #logger.debug("ORIG:UPDATE: %s, %s" %(str(self.index), str(self.data)))
+        if(okey < key):
+            self._upheap(index)
+        #logger.debug("UP:UPDATE: %s, %s" %(str(self.index), str(self.data)))
+        if(okey > key):
+            self._downheap(index)
+        #logger.debug("DOWN:UPDATE: %s, %s" %(str(self.index), str(self.data)))
 
     def update(self, key, item):
         assert item in self.index
@@ -104,60 +75,81 @@ class BinaryHeap(Heap):
 
     def add(self, cost, item):
         #logger.debug("ORIG:ADD %s, %s, %d" %(str(self.index), str(self.data), len(self.data)))
-        assert len(self.data) >= self.size
         if item not in self.index:
-            self.size += 1
-            if len(self.data) >= self.size:
-                self.data[self.size - 1] = (cost, item)
-            else:
-                self.data.append((cost, item))
-            self.index[item] = self.size - 1
-            self._upheap(self.size - 1)
+            i = len(self.data)
+            self.data.append((cost, item))
+            self.index[item] = i 
+            self._upheap(i)
         else:
             self._update(cost, item)
-        #logger.debug("ADD %s, %s, %d" %(str(self.index), str(self.data[0:self.size]), self.size))
+        #logger.debug("ADD %s, %s" %(str(self.index), str(self.data)))
 
     def findmin(self):
-        if self.size == 0:
+        if len(self.data) == 0:
             return None, None
         return self.data[0]
 
+    def empty(self):
+        return len(self.data) == 0
+
     def deleteMin(self):
-        #logger.debug("ORIG:REMOVE %s, %s, %d" %(str(self.index), str(self.data[0:self.size]), self.size))
-        cost, item = self.data[0]
-        assert item in self.index
-        del self.index[item]
-        if(self.size == 1):
-            self.size = 0
-        else:
-            (lcost, litem) = self.data[self.size - 1]
+        #logger.debug("ORIG:REMOVE %s, %s" %(str(self.index), str(self.data)))
+        lcost, litem = self.data.pop()
+        if(len(self.data) != 0):
+            cost, item = self.data[0]
             self.data[0] = (lcost, litem)
             self.index[litem] = 0 
-            self.size = self.size - 1
             self._downheap(0)
-        #logger.debug("REMOVE %s, %s, %d" %(str(self.index), str(self.data[0:self.size]), self.size))
+            #logger.debug("REMOVE %s, %s" %(str(self.index), str(self.data)))
+        else:
+            cost, item = lcost, litem
+        assert item in self.index
+        del self.index[item]
         return cost, item
 
     def check(self):
-        for i in range(self.size):
+        return 
+        size = len(self.data)
+        for i in range(size):
             cost, item = self.data[i]
             assert item in self.index
             assert self.index[item] == i
-            if self._parent(i) >= 0:
-                assert cost >= self.data[self._parent(i)][0]
-            if self._child(i, True) >= 0:
-                assert cost <= self.data[self._child(i, True)][0]
-            if self._child(i, False) >= 0:
-                assert cost <= self.data[self._child(i, False)][0], "%d->%d:%s, %s" %(i, self._child(i, False), str(cost), str(self.data[self._child(i, False)]))
+            if i != 0:
+                assert cost >= self.data[(i-1)//2][0]
+            if 2*i + 1 < size:
+                assert cost <= self.data[2*i + 1][0], "%d->%d:%s, %s" %(i, 2*i + 1, str(cost), str(self.data))
+                if 2*i + 2 < size:
+                    assert cost <= self.data[2*i + 2][0], "%d->%d:%s, %s" %(i, 2*i+2, str(cost), str(self.data[2*i + 2]))
         for key, value in self.index.items():
             assert key == self.data[value][1]
 
     def __repr__(self):
-        return str(self.data[0:self.size])
+        return str(self.data)
+
+from heapq import heappush, heappop
+class BinaryHeapq(object):
+    def __init__(self):
+        self.heap = []
+    def update(self, cost, item):
+        raise NotImplementedError
+    def add(self, cost, item):
+        heappush(self.heap, (cost, item)) 
+    def delete(self, item):
+        raise NotImplementedError
+    def deleteMin(self):
+        return heappop(self.heap) 
+    def findmin(self):
+        raise NotImplementedError
+    def merge(self, h):
+        raise NotImplementedError
+    def empty(self):
+        return len(self.heap) == 0
+    def check(self):
+        pass
 
 if __name__ == "__main__":
     h = BinaryHeap()
-    N = 100
+    N = 1000000
     logging.basicConfig(filename='heap.txt', level=logging.INFO)
     logger.disabled = True
     items = [random.randint(1, 1000) for i in range(N)]
